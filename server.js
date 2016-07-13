fc27f8ac20bdf1a26c511db3bda48f8e6d38d4e4
Martin Stofko <martinstofko@icloud.com>
2016-07-12 21:36:35 -0400

    Initial app setup.
    Created package file. Setup simple node.js server.

---
+++ server.js
@@ -0,0 +1,63 @@
+var express = require("express");
+var path = require("path");
+
+// set app variables
+var app = express();
+app.set("port", 8080);
+
+// set static middleware
+app.use(express.static(path.join(__dirname, "/")));
+
+// get planets
+app.get("/api/planets", function(req, res) {
+	res.json(planets);
+});
+
+// get planet by id
+app.get("/api/planets/:id", function(req, res){
+	var id = parseInt(req.params.id);
+	var planet = {};
+	// loop through planets
+	for (var i = 0; i < planets.length; i++) {
+		if (planets[i].id === id) {
+			planet = planets[i];
+			break;
+		}
+	}
+	res.json(planet);
+});
+
+// start server
+var server = app.listen(app.get("port"), function() {
+	var port = server.address().port;
+	console.log("Listening on port " +  port);
+	console.log("Use ^c to stop the server");
+});
+
+// planets array
+var planets = [
+	{
+		id: 1,
+		name: "Mercury",
+		color: "gray",
+		radius: 2
+	},
+	{
+		id: 2,
+		name: "Venus",
+		color: "yellow",
+		radius: 4
+	},
+	{
+		id: 3,
+		name: "Earth",
+		color: "blue",
+		radius: 5
+	},
+	{
+		id: 4,
+		name: "Mars",
+		color: "red",
+		radius: 3
+	}
+];
