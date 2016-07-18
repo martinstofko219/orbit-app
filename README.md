3672c62a123c7c7c37b3cbd6d5902133cd50c45f
Martin Stofko <martinstofko@icloud.com>
2016-07-18 09:09:13 -0400

    Implemented a view and setup routing.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # orbit-app
-A simple orbiting spheres app using AngularJS.
+A simple orbiting spheres app using AngularJS, jQuery, Bootstrap and scss preprocessor.
+
+The backend dummy API is written in node.js with express.
+
+Build tasks are handled through npm scripts using cli commands.
+
+Run npm install to install all dependencies. Run npm start to start the node.js server.
--- public/app/app.js
+++ public/app/app.js
@@ -1,3 +1,13 @@
 (function() {
-    var app = angular.module("orbitApp", []);
+    var app = angular.module("orbitApp", ["ngRoute"]);
+
+    app.config(["$routeProvider", function($routeProvider) {
+    	// configure routes
+    	$routeProvider
+    		.when("/", {
+    			controller: "orbitController",
+    			templateUrl: "app/views/main.html"
+    		})
+    		.otherwise({ redirectTo: "/" });
+    }]);
 }());
